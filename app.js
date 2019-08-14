c33c6d50624f9a6972de5ee0fc5e14d8af2946cb
Tom Steinmeier <neuro.genesis67@gmail.com>
2019-08-14 19:46:02 +0200

    heroku testing - hbs css

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use('/welcome', cardsRouter);
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.render('cards/welcome/welcome.hbs');
