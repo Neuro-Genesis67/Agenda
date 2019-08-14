f163d3b2c6345f6333aa2aced642da95ab8ef30a
Tom Steinmeier <neuro.genesis67@gmail.com>
2019-08-14 18:43:41 +0200

    heroku testing
    routes

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const cardsRouter = require('./routes/cards');
+const port = process.env.PORT || 3000;
+
 const app = express();
+app.use('/welcome', cardsRouter);
 
 app.get('/', (req, res) => {
     res.send('<h1> Agenda </h1>')
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => console.log(`Server listening on port `));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO
+// -Integrate and test the following:
+// |      | routes
+// |      | hbs
+// |      | mongoose
+// |      | dotenv
+// |      | body-parser
+//
+//
