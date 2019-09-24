8b88fd5d2d8f992f42ab76b40cb2a885b1571edb
Tom Steinmeier <neuro.genesis67@gmail.com>
2019-09-24 16:33:37 +0200

    removed some stuff

--- Backup/posting/form.js
+++ Backup/posting/form.js
@@ -1,17 +1,17 @@
-<form id="form" method="post" action="/router/testPost">
-    <input type="text" name="data1" value="________">
-    <input type="text" name="data2" value="________">
-    <input id="btnSubmit" type="submit" value="submit">
-</form>
-
-
-
-
-function submitAndRedirect() {
-    let formSubmit = document.getElementById('form');
-    formSubmit.submit();
-    setTimeout(() => {
-        window.location.replace("router/welcome2");
-        return false;
-    }, 10);
-}
+// <form id="form" method="post" action="/router/testPost">
+//     <input type="text" name="data1" value="________">
+//     <input type="text" name="data2" value="________">
+//     <input id="btnSubmit" type="submit" value="submit">
+// </form>
+//
+//
+//
+//
+// function submitAndRedirect() {
+//     let formSubmit = document.getElementById('form');
+//     formSubmit.submit();
+//     setTimeout(() => {
+//         window.location.replace("router/welcome2");
+//         return false;
+//     }, 10);
+// }
